99ec5640f25169595142f7e189b8d0bfdb90cb50
IvanRuskevych <ivan.ruskevych@gmail.com>
2023-11-05 14:22:05 +0200

    PostgreSQL: created db_sls & table users

---
+++ 09_auth_api/db.js
@@ -0,0 +1,14 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  user: process.env.PGS_USER,
+  password: process.env.PGS_PSW,
+  host: process.env.PGS_HOST,
+  port: process.env.PGS_PORT,
+  database: process.env.PGS_DB,
+});
+
+client.connect();
+client
+  .query()
+  .then((res) => console.log("DB connected successfully" + res.rows[0]));
